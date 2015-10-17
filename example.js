6eabcfe9b718341875756d1de30b37442bb4fa33
sethvincent <sethvincent@gmail.com>
2015-10-17 00:06:34 -0700

    refactor module to check if point is inside boundaries, clean neighborhoods and city council districts, make data requireable

---
+++ example.js
@@ -0,0 +1,3 @@
+var boundaries = require('./index')
+
+console.log(boundaries(-122.345002, 47.667044))
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-module.exports = {
-  'census-tracts': require('./data/census-tracts.geojson'),
-  'city-council-districts': require('./data/city-council-districts.geojson'),
-  'city-limits': require('./data/city-limits.geojson'),
-  neighborhoods: require('./data/neighborhoods.geojson'),
-  parks: require('./data/seattle-parks-osm.geojson'),
-  'zip-codes': require('./data/zip-codes.geojson')
+var turf = require('turf')
+var flatten = require('lodash.flatten')
+var data = require('./data')
+
+module.exports = function boundaries (long, lat) {
+  var point = turf.point([long, lat])
+
+  return flatten(Object.keys(data).map(function (key) {
+    return data[key].features
+      .filter(function (boundary) {
+        return turf.inside(point, boundary)
+      })
+      .map(function (boundary) {
+        var result = {}
+        result[key] = boundary
+        return result
+      })
+  }))
 }
